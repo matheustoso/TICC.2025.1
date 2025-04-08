3629cf02f457360f1b3d0b456a3fd0257518b7a7
Matheus Toso <matheus.toso82@gmail.com>
2025-04-07 21:36:58 -0300

    [TGA] - feat: Cria codificação Elias-Gamma

---
+++ TGA/services/eliasgamma.py
@@ -0,0 +1,50 @@
+import math
+
+prefix = "0"
+stop_bit = "1"
+
+def encode(input, alphabet) -> str:
+    output = ""
+    
+    for char in input:
+        symbol = alphabet[char]
+        
+        n = math.floor(math.log2(symbol))
+        remainder = symbol - 2 ** n
+        
+        suffix = f"{remainder:b}".zfill(n)
+
+        codeword = prefix * n + stop_bit + suffix
+        output += codeword
+    
+    return output
+
+def decode(input, alphabet) -> str:
+    alphabet = {v: k for k, v in alphabet.items()}
+    
+    output = ""
+    
+    suffix = ""
+    n = 0
+    is_prefix = True
+    
+    for bit in input:
+        
+        if is_prefix:
+            if bit == prefix:
+                n += 1
+            else:
+                is_prefix = False
+            
+        else:
+            suffix += bit
+            
+            if len(suffix) == n:
+                symbol = 2 ** n + int(suffix, 2)
+                output += alphabet[symbol]
+                
+                suffix = ""
+                n = 0
+                is_prefix = True
+    
+    return output
